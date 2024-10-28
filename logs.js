9fbcb0470dcd45ec675b379256e8abaeb22527b2
Tyler731137 <161597242+Tyler731137@users.noreply.github.com>
2024-10-28 21:19:12 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 
-const time = new Date();
+const time = new Date(); // Get the current date and time
 
 async function appendLogsSuccess(fileName, dropboxLink, uniqueId, customLink) {
+    // Construct the success log message with relevant information
     const SuccessMsg = `                    ------------------------------------
                     file uploaded successfully at ${time}
                     file name: ${fileName}
@@ -10,7 +11,7 @@ async function appendLogsSuccess(fileName, dropboxLink, uniqueId, customLink) {
                     dropbox link: ${dropboxLink}
                     unique id: ${uniqueId}
                     ------------------------------------\n`;
-
+    // Append the success message to the logs.txt file
     fs.appendFile('./logs.txt', SuccessMsg, err => {
         if (err) {
             console.error('Error writing to logs file:', err);
@@ -21,13 +22,14 @@ async function appendLogsSuccess(fileName, dropboxLink, uniqueId, customLink) {
 }
 
 async function appendLogsFailure(fileName, uniqueId, error) {
+    // Construct the Failure log message with relevant information
     const FailureMsg = `                    ------------------------------------
                     file upload attempt at ${time}
                     file name: ${fileName}
                     unique id: ${uniqueId}
                     reason: ${error}
                     ------------------------------------\n`;
-
+    // Append the failure message to the logs.txt file
     fs.appendFile('./logs.txt', FailureMsg, err => {
         if (err) {
             console.error('Error writing to logs file:', err);
